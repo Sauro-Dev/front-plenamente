9d0848f5ce10735b37400ca1f5b4fc844042b643
BMarin <bryanmarin2003@hotmail.com>
2024-08-17 03:27:27 -0500

    feat: Ts trabajado e integrado al html, obviamente tambien se trabajó el auth.service

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../enviroment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = `${environment.apiUrl}/users/login`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const loginRequest = { username, password };
+    return this.http.post<any>(this.apiUrl, loginRequest);
+  }
 }
