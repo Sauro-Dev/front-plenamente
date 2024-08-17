2ba07c18664f1988030da7224a95032a256a93d3
BMarin <bryanmarin2003@hotmail.com>
2024-08-17 01:43:20 -0500

    feat: Estructura html y estilo tailwind para el inicio de sesión creados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
