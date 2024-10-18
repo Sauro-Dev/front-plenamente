97b0a705f0f574a4071096afe1ea66551504e097
Tunkifloo <nicolocisneros@gmail.com>
2024-10-18 14:08:36 -0500

    feat: Correción de rutas hijas de Users y ajuste del service para registro de usuarios

--- src/app/components/users/users.routes.ts
+++ src/app/components/users/users.routes.ts
@@ -5,21 +5,19 @@ export default [
     path: '',
     loadComponent: () =>
       import('./users/users.component').then((m) => m.UsersComponent),
-    children: [
-      {
-        path: 'register',
-        loadComponent: () =>
-          import('./register/register.component').then(
-            (m) => m.RegisterComponent
-          ),
-      },
-      {
-        path: 'details/:id',
-        loadComponent: () =>
-          import('./users-details/users-details.component').then(
-            (m) => m.UsersDetailsComponent
-          ),
-      },
-    ],
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: 'details/:id',
+    loadComponent: () =>
+      import('./users-details/users-details.component').then(
+        (m) => m.UsersDetailsComponent
+      ),
   },
 ] as Routes;
